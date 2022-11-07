87dc19b56bc256e370137299a60555f4367bbe7b
RufusMinor <emyznikov74@gmail.com>
2022-11-07 11:34:29 +0500

    update

--- app/src/main/java/com/example/alldevices/Control.java
+++ app/src/main/java/com/example/alldevices/Control.java
@@ -4,20 +4,24 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.alldevices.databinding.FragmentControlBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -38,13 +42,15 @@ DatabaseReference mDatabase, mDatabaseChange;
                              Bundle savedInstanceState) {
 
         mDatabase= FirebaseDatabase.getInstance().getReference();
-        mDatabaseChange=mDatabase.child(date)
+        mDatabaseChange=mDatabase.push();
+        String key=mDatabaseChange.getKey();
+        //mDatabaseChange=mDatabase.child(date);
 
 
         binding=FragmentControlBinding.inflate(inflater,container,false);
         View root=binding.getRoot();
         addVerefication();
-        equalsDate();
+        equalsDate(root);
         add=(FloatingActionButton)root.findViewById(R.id.floating_action_button);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,14 +87,37 @@ DatabaseReference mDatabase, mDatabaseChange;
         dialog=builder.create();
     }
 //Сравнение дат с БД
-    public void equalsDate(){
+    public void equalsDate(View view){
         String date1 = new SimpleDateFormat("dd.MM.yyyy").format(new Date());
         Log.d("Date","На: "+date1);
 
 
-        Query s=mDatabase.child("corrector").orderByChild("date");
-        int result=date1.compareTo(String.valueOf(s));
-        text2.setText(result);
+        Query query=mDatabase.orderByChild("date").equalTo(date1);
+        query.addValueEventListener(new ValueEventListener(){
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot){
+                Log.d("Date","Метод работает");
+                if(snapshot.exists()){
+                    Log.d("Date","Совпадение есть!");
+                    for(DataSnapshot ds: snapshot.getChildren()){
+                        Log.d("Date","На: ");
+                        String key= ds.child("key").getValue(String.class);
+                        text.setText(key);
+                    }
+                }
+                else {
+                    Log.d("Date","Совпадений нет");
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error){
+                Log.d("Date","ошибка");
+            }
+        });
+
+
+
+
 
 
     }
